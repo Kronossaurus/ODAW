d1cce9cb3d8f07b8fe7b9f96c7f779c4fd8caf74
Kronossaurus <brvictor.chess@hotmail.com>
2016-04-30 04:10:37 -0300

    Início do exercício de mysql com formulários

---
+++ mysql.php
@@ -0,0 +1,26 @@
+<!--
+<?php
+	//teste de conexão mysql
+	if (!$conexao = mysql_connect('localhost','root','')) {
+		die('Não conectou');
+	}
+	$bd = 'hotel';
+	$query = "CREATE DATABASE ".$bd;
+	if (!mysql_query($query)) {
+		die('Erro na consulta');
+	}
+	else{
+		echo 'Executou a consulta';
+	}
+	mysql_select_db($bd,$conexao);
+?>
+-->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP + MySQL</title>
+</head>
+<body>
+
+</body>
+</html>
