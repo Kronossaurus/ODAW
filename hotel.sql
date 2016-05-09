8ac4590b7dba3e6f887698c88c0c758414de0e8c
Kronossaurus <brvictor.chess@hotmail.com>
2016-05-09 20:40:18 -0300

    Added AUTO_INCREMENT

--- hotel.sql
+++ hotel.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE empregados
 (
     nome varchar(100),
-    codEmp integer NOT NULL,
+    codEmp integer NOT NULL AUTO_INCREMENT,
     CONSTRAINT empregados_pkey PRIMARY KEY (codEmp)
 );
 
 CREATE TABLE hoteis
 (
-    codHotel integer NOT NULL,
+    codHotel integer NOT NULL AUTO_INCREMENT,
     nome varchar(100),
     endereço varchar(250),
     telefone integer,
@@ -16,7 +16,7 @@ CREATE TABLE hoteis
 
 CREATE TABLE tipoQuarto
 (
-    id integer NOT NULL,
+    id integer NOT NULL AUTO_INCREMENT,
     nome varchar(100),
     camaExtra bool,
     CONSTRAINT tipoQuarto_pkey PRIMARY KEY (id)
@@ -37,7 +37,7 @@ CREATE TABLE quartos
 
 CREATE TABLE servicos
 (
-    codS integer NOT NULL,
+    codS integer NOT NULL AUTO_INCREMENT,
     tipoS varchar(100),
     preco double,
     CONSTRAINT servicos_pkey PRIMARY KEY (codS)
@@ -45,7 +45,7 @@ CREATE TABLE servicos
 
 CREATE TABLE clientes
 (
-    nRegistro integer NOT NULL,
+    nRegistro integer NOT NULL AUTO_INCREMENT,
     nome varchar(100),
     endereço varchar(250),
     telefone integer,
@@ -56,7 +56,7 @@ CREATE TABLE estadias
 (
     dtCIn date,
     dtCOut date,
-    codEstadia integer NOT NULL,
+    codEstadia integer NOT NULL AUTO_INCREMENT,
     codHotel integer NOT NULL,
     nRegistro integer NOT NULL,
     CONSTRAINT estadias_fkey FOREIGN KEY (nRegistro)
@@ -90,7 +90,7 @@ CREATE TABLE limpeza
         REFERENCES quartos (nQrto,codHotel) MATCH SIMPLE,
     CONSTRAINT limpeza_fkey2 FOREIGN KEY (codEmp)
         REFERENCES empregados (codEmp) MATCH SIMPLE,
-    CONSTRAINT limpeza_pkey PRIMARY KEY (nQrto,codEmp)
+    CONSTRAINT limpeza_pkey PRIMARY KEY (nQrto,codEmp,dataLimpeza)
 );
 
 CREATE TABLE oferece
@@ -103,7 +103,7 @@ CREATE TABLE oferece
         REFERENCES servicos (codS) MATCH SIMPLE,
     CONSTRAINT oferece_fkey2 FOREIGN KEY (codEstadia)
         REFERENCES estadias (CodEstadia) MATCH SIMPLE,
-    CONSTRAINT oferece_pkey PRIMARY KEY (codS,codEstadia)
+    CONSTRAINT oferece_pkey PRIMARY KEY (codS,codEstadia,dataServico,horaServico)
 );
 
 CREATE TABLE preco
