01bcb0e18353250bdf33d828594d50477b076f97
Kronossaurus <brvictor.chess@hotmail.com>
2016-06-18 18:16:02 -0300

    Add arquivo de conexão

---
+++ Trabalho Final/crudCliente.php
@@ -0,0 +1,41 @@
+<?php
+	include_once 'conexão.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>CRUD de Clientes</title>
+</head>
+<body>
+	<form name="formClientes" method="POST" action="insereCliente.php">
+		<table align="center">
+			<tr>
+				<td colspan="2" align="center">Inserir Clientes</td>
+			</tr>
+			<!--<tr>
+				<td>Código:</td>
+				<td><input type="text" name="idCliente"></input></td>
+			</tr>-->
+			<tr>
+				<td>Nome:</td>
+				<td><input type="text" name="nomeCliente" autocomplete="off"></input></td>
+			</tr>
+			<tr>
+				<td>Endereço:</td>
+				<td><input type="text" name="enderecoCliente" autocomplete="off"></input></td>
+			</tr>
+			<tr>
+				<td>Telefone:</td>
+				<td><input type="text" name="foneCliente" autocomplete="off"></input></td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<input type="reset"></input>
+					<input type="submit" formaction="insereCliente.php"></input>
+				</td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
---
+++ Trabalho Final/insereCliente.php
@@ -0,0 +1,20 @@
+<?php
+	include_once 'conexão.php';
+	//$nRegistro = $_POST['idCLiente'];
+	$nome = $_POST['nomeCliente'];
+	$endereço = $_POST['enderecoCliente'];
+	$telefone = $_POST['foneCliente'];
+	
+	$query = sprintf("SELECT insereCliente('%s', '%s', %d);",
+		$mysqli->real_escape_string($nome),
+		$mysqli->real_escape_string($endereço),
+		$telefone);
+	
+	$result = $mysqli->query($query);
+	if (!$result) {
+		die("Erro na inserção");
+		echo mysqli_error();
+	}
+	else
+		echo "Sucesso";
+?>
